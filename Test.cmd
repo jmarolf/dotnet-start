8621da126d0e932b20b30603cb035a6426b08cc2
Jonathon Marolf <jmarolf@microsoft.com>
2022-01-31 19:21:43 -0800

    sample nuget package

---
+++ Test.cmd
@@ -0,0 +1,3 @@
+@echo off
+
+dotnet test --no-restore --no-build --blame --blame-hang-dump-type full  --blame-hang-timeout 10min --blame-crash-dump-type full --blame-crash-collect-always true --logger "trx;LogFileName=TestResults.trx" -p:ParallelizeTestCollections=true --collect:"XPlat Code Coverage" --results-directory artifacts/test_results
